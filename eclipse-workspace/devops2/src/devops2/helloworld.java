64fc5872a5a0c79cd72a65d8ddbcd51636479997
PC i5 <iaitlmaalem@gmail.com>
2020-01-07 17:36:41 +0100

    commit

---
+++ eclipse-workspace/devops2/src/devops2/helloworld.java
@@ -0,0 +1,13 @@
+package devops2;
+
+public class helloworld
+{
+	public static void main( String[] args )
+    {
+		System.out.println( "Hello World!" );
+		System.out.println( "My Branch" );
+		System.out.println( "My Branch" );  
+
+        System.out.println(Branch2.getResult());
+}
+}
